e1fc43afa4f0c05f55a7090eb3e51e4cd97382a1
depregpsac-lgtm <depregpsac@gmail.com>
2026-03-17 17:29:46 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -38,7 +38,7 @@ async function iniciarSesion(email, password) {
         console.log('🔐 Intentando iniciar sesión...', email);
         
         // Usuario demo hardcodeado para pruebas
-        if (email === 'ministrylion@gmail.com' && password === 'admin') {
+        if (email === 'admin@ministrylion.com' && password === 'admin123') {
             const userDemo = {
                 id: '1',
                 nombre: 'Administrador',
